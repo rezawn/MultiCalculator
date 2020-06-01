0aa142bf57b02602751fab6e37af64efda6d035d
rezawn <rezavengeance00@gmail.com>
2020-06-01 17:58:38 +0700

    kerangka program

---
+++ MultiCal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#SCRIPT UTAMA
+
+from os import system
+system('clear')
+
+print('THIS IS PREVIEW VERSION')
+print()
+
+input('Tekan enter untuk melanjutkan...')
+
+from data import machine
---
+++ data/machine.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#SCRIPT PEMANGGIL
+
+from os import system
+
+while True:
+    system('clear')
+    print("MULTI KALKULATOR")
+    print("""-------------------------------------------------------
+Silahkan pilih jenis kalkulator yang anda inginkan
+1. Kalkulator Numerik [COMING SOON]
+2. Kalkulator Konversi Suhu [COMING SOON]
+3. Kalkulator Konversi Waktu [COMING SOON]
+4. Kalkulator Konversi Mata Uang [COMING SOON]
+5. Kalkulator Berat Badan Ideal [COMING SOON]
+6. Kalkulator Konversi Satuan Panjang [COMING SOON]
+7. Kalkulator Konversi Satuan Berat [COMING SOON]
+8. Kalkulator Konversi Satuan Luas [COMING SOON]
+9. Kalkulator Volume Bangun Ruang [COMING SOON]
+0. Keluar
+-------------------------------------------------------""")
+
+    jenis = input("Respon: ")
+    
+    if jenis == '1':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+            
+    elif jenis == '2':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+            
+    elif jenis == '3':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+    
+    elif jenis == '4':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+    
+    elif jenis == '5':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+    
+    if jenis == '6':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+            
+    elif jenis == '7':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+            
+    elif jenis == '8':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+    
+    elif jenis == '9':
+        system('clear')
+        print('COMING SOON')
+        print()
+        input('Tekan enter untuk keluar...')
+        system('clear')
+        break
+    
+    elif jenis == '0':
+        system('clear')
+        break
