e683ab05ea5ac2debf4892d8bf73a44134a46459
AcintyaUdanArum <66170071+AcintyaUdanArum@users.noreply.github.com>
2020-06-01 20:14:23 +0700

    Update bbi.py
    Pengisian script kalkulator Berat Badan Idea (BBI)

--- data/bbi.py
+++ data/bbi.py
@@ -3,3 +3,111 @@
 
 
 #SCRIPT KALKULATOR BBI
+
+from os import system
+from data import clr
+
+iterasi = True
+while iterasi:
+  system(clr.d)
+  print('KALKULATOR BERAT BADAN IDEAL')
+  print("""-------------------------------------------------------
+Pilih kategori Anda
+1. Bayi 0-12 bulan
+2. Usia 1-10 tahun
+3. Pria
+4. Wanita
+5. Ibu hamil
+0. Kembali
+-------------------------------------------------------""")
+  
+  jenis = input('Respon: ')
+  
+  if jenis == '1':
+    iterasi = True
+    while iterasi:
+      system(clr.d)
+      print('MENGHITUNG BERAT BADAN IDEAL')
+      
+      #usia dalam satuan bulan
+      UB = float(input('Usia Bayi dalam Satuan Bulan : '))
+      
+      Bbi_bayi=(UB/2)+4
+      
+      print("Berat Badan Ideal (kg) : ",Bbi_bayi)
+      ulang = input('Apakah Anda ingin menggunakan kategori ini lagi? (y/n) ')
+      if ulang != 'y':
+        break
+        
+  elif jenis == '2':
+    iterasi = True
+    while iterasi:
+      system(clr.d)
+      print('MENGHITUNG BERAT BADAN IDEAL')
+      
+      #usia dalam satuan tahun
+      UB = float(input('Usia Bayi dalam Satuan Tahun : '))
+      
+      Bbi_anak=(2*UB)+8
+      
+      print("Berat Badan Ideal (kg) : ",Bbi_anak)
+      ulang = input('Apakah Anda ingin menggunakan kategori ini lagi? (y/n) ')
+      if ulang != 'y':
+        break
+        
+  elif jenis == '3':
+    iterasi = True
+    while iterasi:
+      system(clr.d)
+      print('MENGHITUNG BERAT BADAN IDEAL')
+      
+      #Tinggi badan dalam satuan cm
+      TB = float(input('Tinggi Badan dalam Satuan Cm : '))
+      
+      Bbi_pria=(TB-100)-((TB-100)*(10/100))
+      
+      print("Berat Badan Ideal (kg) : ",Bbi_pria)
+      ulang = input('Apakah anda ingin menggunakan kategori ini lagi? (y/n) ')
+      if ulang != 'y':
+        break
+        
+  elif jenis == '4':
+    iterasi = True
+    while iterasi:
+      system(clr.d)
+      print('MENGHITUNG BERAT BADAN IDEAL')
+      
+      #Tinggi badam dalam satuan cm
+      TB = float(input('Tinggi Badan dalam Satuan Cm : '))
+      
+      Bbi_wanita=(TB-100)-((TB-100)*(15/100))
+      
+      print("Berat Badan Ideal (kg) : ",Bbi_wanita)
+      ulang = input('Apakah Anda ingin menggunakan kategori ini lagi? (y/n) ')
+      if ulang != 'y':
+        break
+        
+  elif jenis == '5':
+    iterasi = True
+    while iterasi:
+      system(clr.d)
+      print('MENGHITUNG BERAT BADAN IDEAL')
+      
+      BBI = float(input('Berat Badan Ideal Normal dalam Kg : '))
+      #usia kehamilan dalam satuan minggu
+      UK = float(input('Usia Kehamilan dalam Satuan Minggu : '))
+     
+      Bbi_wanita_hamil=BBI+(UK*0.35)
+      
+      if 0<UK<43:
+        print("Berat Badan Ideal (Kg) : ",Bbi_wanita_hamil)
+        
+       else:
+        print("Usia Kehamilan Harus Berada Diantara 1-42 Minggu")
+        ulang = input('Apakah Anda ingin menggunakan kategori ini lagi? (y/n) ')
+        if ulang != 'y':
+          break
+          
+  elif jenis == '0':
+    system(clr.d)
+    break
