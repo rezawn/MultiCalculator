fd116cfee0d6d9050ea044a0fe1dbdb925a8ed4f
AcintyaUdanArum <66170071+AcintyaUdanArum@users.noreply.github.com>
2020-06-02 22:07:36 +0700

    Update bbi.py

--- data/bbi.py
+++ data/bbi.py
@@ -101,6 +101,9 @@ Pilih kategori Anda
       
       if 0<UK<43:
         print("Berat Badan Ideal (Kg) : ",Bbi_wanita_hamil)
+        ulang = input('Apakah Anda ingin menggunakan kategori ini lagi? (y/n) ')
+        if ulang != 'y':
+          break
         
       else:
         print("Usia Kehamilan Harus Berada Diantara 1-42 Minggu")
