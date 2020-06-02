147bb57123d0a21e04d15330df7ed676688d8568
widya habsari <66183774+widyahabsari@users.noreply.github.com>
2020-06-02 14:09:30 +0700

    Update volume.py
    Pengisian script kalkulator perhitungan volume

--- data/volume.py
+++ data/volume.py
@@ -3,3 +3,132 @@
 
 
 #SCRIPT KALKULATOR VOLUME
+
+from os import system
+from data import clr
+
+while True:
+    system(clr.d)
+    print('KALKULATOR VOLUME BANGUN RUANG')
+    print("""----------------------------------------------
+Pilih Bangun Ruang :
+1. Kubus
+2. Balok
+3. Prisma Segitiga
+4. Limas Segitiga
+5. Limas Segiempat
+6. Tabung
+7. Kerucut
+8. Bola
+0. Keluar
+-----------------------------------------------------------""")
+
+    jenis = input('Bangun Ruang: ')
+
+    while True:
+        if jenis == '1':
+            system(clr.d)
+            print('MENGHITUNG VOLUME KUBUS')
+            s=float(input('sisi:'))
+            v=s**3
+            print('Volume Kubus: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+    
+        elif jenis == '2':
+            system(clr.d)
+            print('MENGHITUNG VOLUME BALOK')
+            p=float(input('panjang:'))
+            l=float(input('lebar:'))
+            t=float(input('tinggi:'))
+            v=p*l*t
+            print('Volume Balok: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+    
+        elif jenis == '3':
+            system(clr.d)
+            print('MENGHITUNG VOLUME PRISMA SEGITIGA')
+            a=float(input('alas:'))
+            t=float(input('tinggi alas segitiga:'))
+            T=float(input('tinggi prisma:'))
+            LA=(a*t)/2
+            v=LA*T
+            print('Volume Prisma Segitiga: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+
+        elif jenis == '4':
+            system(clr.d)
+            print('MENGHITUNG VOLUME LIMAS SEGITIGA')
+            a=float(input('alas:'))
+            t=float(input('tinggi alas segitiga:'))
+            T=float(input('tinggi limas:'))
+            LA=(a*t)/2
+            v=(LA*T)/3
+            print('Volume Limas Segitiga: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+ 
+        elif jenis == '5':
+            system(clr.d)
+            print('MENGHITUNG VOLUME LIMAS SEGIEMPAT')
+            p=float(input('panjang alas:'))
+            l=float(input('lebar alas:'))
+            T=float(input('tinggi limas:'))
+            LA=p*l
+            v=(LA*T)/3
+            print('Volume Limas Segitiga: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+    
+        elif jenis == '6':
+            system(clr.d)
+            print('MENGHITUNG VOLUME TABUNG')
+            r=float(input('jari-jari:'))
+            T=float(input('tinggi tabung:'))
+            LA=(22/7.0)*r*r
+            v=LA*T
+            print('Volume Tabung: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+    
+        elif jenis == '7':
+            system(clr.d)
+            print('MENGHITUNG VOLUME KERUCUT')
+            r=float(input('jari-jari:'))
+            T=float(input('tinggi kerucut:'))
+            LA=(22/7.0)*r*r
+            v=(LA*T)/3
+            print('Volume Kerucut: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+    
+        elif jenis == '8':
+            system(clr.d)
+            print('MENGHITUNG VOLUME BOLA')
+            r=float(input('jari-jari:'))
+            v=(4*(22/7.0)*r**3)/3
+            print('Volume Bola: ',v)
+            print()
+            ulang = input('Apakah anda ingin menggunakan konversi ini lagi? (y/n) ')
+            if ulang != 'y':
+                break
+
+        elif jenis == '0':
+            system(clr.d)
+            break
